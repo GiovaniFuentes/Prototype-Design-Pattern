7da1ffdf89b0435ac884929c9313cebbf2efad22
Giovani Fuentes <giovanifuentes@giovaniff100.nmsu.edu>
2024-05-09 11:17:35 -0600

    changed grocery list example

--- TestCloning.java
+++ TestCloning.java
@@ -1,21 +1,39 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class TestCloning {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         CloneFactory recipeMaker =  new CloneFactory();
         groceryList firstList = new groceryList();
         groceryList clonedList = (groceryList) recipeMaker.getClone(firstList);
 
+       
+        PrintWriter output = new PrintWriter("output.txt");
+        
+        output.println(firstList);
 
-        System.out.println(firstList);
-
-        System.out.println(clonedList);
-
-
-        System.out.println("\n\nThe Following proves that the cloned recipe is in a different location in memory from the first");
-        System.out.println("First List Hashcode: " + System.identityHashCode(firstList));
-
-        System.out.println("Cloned List Hashcode: " + System.identityHashCode(clonedList));
+        output.println(clonedList);
 
 
+        output.println("\n\nThe Following proves that the cloned recipe is in a different location in memory from the first");
+        output.println("First List Hashcode: " + System.identityHashCode(firstList));
+
+        output.println("Cloned List Hashcode: " + System.identityHashCode(clonedList));
+
+        output.println("\nSetting the cloned Lists's first item to\nmilk and setting the the name to Bob.");
+        clonedList.setItem1("milk");
+        clonedList.setName("Bob");
+
+        output.println("\nPrinting both lists after the change");
+        output.println("first list");
+        output.println(firstList);
+        output.println("\nCloned list");
+        output.println(clonedList);
+
+
+
+
+        output.close();
     }
 }
